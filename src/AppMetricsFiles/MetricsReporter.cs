fdf3e97b55118591fd5acfa97c62cb7971356c2d
edtshuma <edtshuma@gmail.com>
2023-01-26 08:39:17 +0100

    Add R(E)D Error metrics

--- src/AppMetricsFiles/MetricsReporter.cs
+++ src/AppMetricsFiles/MetricsReporter.cs
@@ -9,6 +9,7 @@ public class MetricReporter
 {
     private readonly ILogger<MetricReporter> _logger;
     private readonly Counter _requestCounter;
+    private readonly Gauge _requestGauge;
     private readonly Histogram _responseTimeHistogram;
 
     public MetricReporter(ILogger<MetricReporter> logger)
@@ -16,6 +17,7 @@ public class MetricReporter
         _logger = logger ?? throw new ArgumentNullException(nameof(logger));
 
         _requestCounter = Metrics.CreateCounter("total_requests", "The total number of requests serviced by this API.");
+        _requestGauge = Metrics.CreateGauge("total_requests_gauge", "The total number of requests serviced by this API.");
 
         _responseTimeHistogram = Metrics.CreateHistogram("request_duration_seconds",
             "The duration in seconds between the response to a request.", new HistogramConfiguration
@@ -28,6 +30,7 @@ public class MetricReporter
     public void RegisterRequest()
     {
         _requestCounter.Inc();
+        _requestGauge.Inc();
     }
 
     public void RegisterResponseTime(int statusCode, string method,string path, TimeSpan elapsed)
